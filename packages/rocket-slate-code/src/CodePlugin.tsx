f856cf72438b98cceb925289406ee8b9bdb4c639
Святослав Пожидаев <svyatreal@gmail.com>
2020-02-26 22:40:15 +0300

    code editor

--- packages/rocket-slate-code/src/CodePlugin.tsx
+++ packages/rocket-slate-code/src/CodePlugin.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import styled from 'styled-components';
-import { RenderElementProps } from 'slate-react';
+import { Transforms, Editor } from 'slate';
+import { RenderElementProps, ReactEditor, useSlate } from 'slate-react';
 import { CODE, CodePlugin } from 'slate-plugins-next';
 import EditorCode from 'react-simple-code-editor';
 import { IRocketSlatePlugin } from '@rocket-slate/core/Editor';
 
 const CodeWrap = styled.div`
   font-family: monospace;
-  background-color: #eee;
+  background-color: #f7f7f7;
   font-size: 1em;
   padding: 0.25em 0.4em;
   border-radius: 2px;
@@ -19,23 +20,23 @@ const RocketSlateCodeBlock = (
     highlight: (value: string) => string | React.ReactNode;
   },
 ) => {
+  const editor = useSlate();
   const { element, children, attributes, highlight = (code) => code } = props;
-  const [code, setCode] = useState('function() { return 1 };');
+  const { children: [{ text }] } = element;
+  const path = useMemo(() => ReactEditor.findPath(editor, element), [editor, element]);
+  const [code, setCode] = useState('');
+  console.log('props', props);
   return (
-    <div {...attributes} data-slate-type={CODE} contentEditable={false}>
-      <div>
-        <input />
-        <EditorCode
-          value={code}
-          onValueChange={(code) => {
-            console.log('setCode');
-            setCode(code);
-          }}
-          highlight={highlight}
-        />
-      </div>
-      {children}
-    </div>
+    <CodeWrap {...attributes} data-slate-type={CODE} contentEditable={false}>
+      <EditorCode
+        value={code || ""}
+        onValueChange={(code) => {
+          Editor.insertText(editor, code);
+          setCode(code);
+        }}
+        highlight={highlight}
+      />
+    </CodeWrap>
   );
 };
 
